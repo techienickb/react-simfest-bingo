6ef5783c5682bd5f4cdec2641988a4f5c0923c95
Nick Brown <nick@nbdev.co.uk>
2021-11-11 21:29:19 +0000

    Added Captail Tailstrike

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ const data = [
   '✈️ Free 🌟', 'Pleasing', 'Lovely scenery 🏔️', 'Willy waving', 'Flamingo 🦩', 'Pink ❣️', 'Yetis cabin service manager', "I'm a pilot 🧑‍✈️", 'Airbus', 'Maintenance required 🔧',
   'The Lullaby trust will not be happy with that ❌', 'The Lullaby trust will be happy with that ☑️', 'Simfest ATC truck', 'Only on Vatsim', 'Booze Cruise 🍸',
   'Dick in the rear', 'Hot tub 💦', 'An incident has occurred', 'Diplomatic Incident 🚫', 'UMT (Uninvited Male Touching)', 'ALEXA Shut Up 🙊', "Happy 😄",
-  "Grumpy 😠", 'Give Away', 'Raid', 'simfestprizes@gmail.com', 'That smells awful ☣️', 'GET OUT', 'Pass the QRH', "Horgy's height 📏", "Go-around 🛫", "Hold 🔁",
+  "Grumpy 😠", 'Give Away', 'Raid', 'That smells awful ☣️', 'GET OUT', 'Pass the QRH', "Horgy's height 📏", "Go-around 🛫", "Hold 🔁",
   "Pot of pleasure, Urn of joy", "This is nice", "Simon Kelsey Brief", "Smug look 😏", "Someone is doing that", "Butter 🧈", "Floater 🛬", "Blindfolded landing 🧑‍🦯",
   'A playing of "don\'t show keith (or chat) your teeth"', 'Flaggpunsh 🍶', 'How much the Sim cost? 💸', 'Nothing to see here 🙈', 'Are you using MSFS?', 'Pardon ⁉️', 'Cabin Phone Call',
   'Has anyone checked the wings for ice? ❄️', 'Fatal Damage 💥', 'Throffy coffee ☕', 'Someone can\'t see/no contacts', 'Beep Beep Beep', 'What can Horgy reach',
   'Moist ☔', 'Merch Daddy 🎁👴', 'Muff 🙊', 'Give-away Daddy 🎁', 'Has Horgy finished his sim yet? ⏲️', 'Importing/exporting metal tubes ↔️', 'Horgy sound board 🗣️',
-  "We've broken someone", 'Anti-Faff', 'Faff', 'VRB ⚡'
+  "We've broken someone", 'Anti-Faff', 'Faff', 'VRB ⚡',  'Captain Tailstrike 🛫'
 ];
 
 function App() {
